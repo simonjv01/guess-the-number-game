0a68e05b4ab29faf4b41f90557c49f9fcf008632
simonjv01 <sjvargas01@gmail.com>
2020-12-30 00:58:26 -0500

    created Appconfig and config package

--- core/src/main/java/academy/learnprogramming/GameImpl.java
+++ core/src/main/java/academy/learnprogramming/GameImpl.java
@@ -14,7 +14,9 @@ public class GameImpl implements Game {
     // == fields ==
     @Autowired
     private NumberGenerator numberGenerator;
-    private int guessCount = 10;
+
+    @Autowired
+    private int guessCount;
     private int number;
     private int guess;
     private int smallest;
@@ -81,7 +83,10 @@ public class GameImpl implements Game {
         return remainingGuesses;
     }
 
-
+    @Override
+    public int getGuessCount() {
+        return guessCount;
+    }
 
     @Override
     public void check() {
--- core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
+++ core/src/main/java/academy/learnprogramming/MessageGeneratorImpl.java
@@ -12,7 +12,7 @@ public class MessageGeneratorImpl implements MessageGenerator{
 
     @Autowired
     private Game game;
-    private  int guessCount = 10;
+
 
     public void init(){
         log.info("game = {}", game);
@@ -35,7 +35,7 @@ public class MessageGeneratorImpl implements MessageGenerator{
             return "You lost. The number was " + game.getNumber();
         } else if(!game.isValidNumberRange()) {
             return "Invalid number range!";
-        } else if (game.getRemainingGuesses() == guessCount) {
+        } else if (game.getRemainingGuesses() == game.getGuessCount()) {
             return "What is your first guess?";
         } else {
             String direction = "Lower";
