ac04f6fa1346b077a22c49f4d4e3fbc642a00361
simonjv01 <sjvargas01@gmail.com>
2021-01-16 00:07:34 -0500

    added spring boot to project

---
+++ console/src/main/java/academy/learnprogramming/Main.java
@@ -0,0 +1,19 @@
+package academy.learnprogramming;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class Main {
+
+    //private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        log.info("Guess The Number Game");
+
+        SpringApplication.run(Main.class, args);
+
+    }
+}
