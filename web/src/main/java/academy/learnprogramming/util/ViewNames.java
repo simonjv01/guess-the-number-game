4a2135235678b3ab4a49267cad1291ca295e4365
simonjv01 <sjvargas01@gmail.com>
2021-01-22 13:35:26 -0500

    created home.html file

---
+++ web/src/main/java/academy/learnprogramming/util/ViewNames.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming.util;
+
+public class ViewNames {
+
+    // ==constants ==
+    public static final String HOME = "home";
+
+    // == constructors ==
+    private ViewNames() {}
+
+
+}
